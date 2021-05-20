77a4cf620ba0b93e353fea8235261ac769a85d21
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2021-05-20 15:48:08 -0400

    Added clock functionality

--- index.js
+++ index.js
@@ -20,4 +20,18 @@ purpleButton.addEventListener('click', () => changeWatch("purple"))
 redButton = document.querySelector('.red')
 redButton.addEventListener('click', () => changeWatch("red"))
 
+let watchDisplay = document.querySelector('.watch-display')
 
+const viewTime = () => {
+    console.log(watchDisplay)
+    const updateClock = () => {
+        let time = new Date().toLocaleTimeString()
+        watchDisplay.appendChild(document.createTextNode(time))
+
+    }
+
+    setInterval(updateClock, 1000)
+}
+
+let timeButton = document.querySelector('.time-btn')
+timeButton.addEventListener('click', () => viewTime())
